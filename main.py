c2bd769245aa15e3f79f3a83b49597bfe96f12c0
Umarbek_Saidov <umarbeksaidov2003@gmail.com>
2022-03-14 19:58:07 +0500

    welcome bug fixed && add author_signature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from aiogram import Bot, types
-import aiogram
-from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
+import aiogram
+from aiogram import Bot, types
 
 b = Bot(token="5204482932:AAGsX5xQxiD0dBvw5On6qx72cGqtPU1Fwk4")
 bot = Dispatcher(b)
@@ -9,7 +9,7 @@ bot = Dispatcher(b)
 
 @bot.message_handler(commands="start")
 async def st(message):
-    await message.reply(f'Salom <b><a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a></b> men ishlayabman', parse_mode='HTML')
+    await message.reply(f'Salom <b><a href="tg://user?id={message.from_user.id}">{message.from_user.first_name}</a></b> men ishlayabman😉', parse_mode='HTML')
 
 
 @bot.message_handler(commands="test")
@@ -21,20 +21,16 @@ async def st(message):
 async def podpis(message):
     if message.chat.type == 'channel':
         t = message.text
-        await message.edit_text(t+f'\n\n<a href="https://t.me/Teran_Pikrlar/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>',
-                                parse_mode='HTML', disable_web_page_preview=True)
+        await message.edit_text(f'{t}\n\n© <b>{message.author_signature}</b>\n\n<a href="https://t.me/{message.chat.username}/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>', parse_mode='HTML', disable_web_page_preview=True)
 
 
-@bot.channel_post_handler(content_types=["photo", "video", "audio", "document"])
+@bot.channel_post_handler(content_types=["photo", "video", "animation", "voice", "audio", "document"])
 async def photopodpis(message):
     if message.chat.type == 'channel':
-        if message.text is not None:
-            t = message.caption
-            await message.edit_caption(f'{t}\n\n<a href="https://t.me/Teran_Pikrlar/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>',
-                                       parse_mode='HTML')
+        if message.caption is not None:
+            await message.edit_caption(f'{message.caption}\n\n© <b>{message.author_signature}</b>\n\n<a href="https://t.me/{message.chat.username}/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>', parse_mode='HTML')
         else:
-            await message.edit_caption(f'\n\n<a href="https://t.me/Teran_Pikrlar/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>',
-                                       parse_mode='HTML')
+            await message.edit_caption(f'© <b>{message.author_signature}</b>\n\n<a href="https://t.me/{message.chat.username}/{message.message_id}">Teran Pikrlaydiganlar Uchun</a>', parse_mode='HTML')
 
 if __name__ == '__main__':
-    executor.start_polling(bot)
+    executor.start_polling(bot, skip_updates=True)
